9e85fbc909c1053f6c3f315674e73c830d848ad9
Emmanuel <cyklonehateka1@gmail.com>
2024-08-21 17:46:58 +0000

    Fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
-import styled from "styled-components";
 import Home from "./pages/home/Home";
 import Dashboard from "./pages/dashboard/Dashboard";
 import Login from "./pages/admin-login/Login";
 
 function App() {
   return (
-    <Container>
-      <Router basename="/">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/login" element={<Login />} />
-        </Routes>
-      </Router>
-    </Container>
+    <Router basename="/">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route path="/admin/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
-
-const Container = styled.div`
-  width: 100vw;
-`;
 
 export default App;
