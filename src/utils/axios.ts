2df8982593383938d0b90da964e93315b531e581
EMMANUEL HATEKA <84219325+cyklonehateka1@users.noreply.github.com>
2025-06-04 15:53:49 +0000

    Update axios.ts

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api/",
+  baseURL: "https://gh-card-finder-server.onrender.com",
   headers: { "X-Custom-Header": "foobar" },
 });
